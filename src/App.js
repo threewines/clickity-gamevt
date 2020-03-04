5d15b25214880ef406579984b24ce9a58aa005fc
unknown <threewines@yahoo.com>
2020-03-03 19:39:22 -0600

    Add existing file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import FriendCard from "./components/FriendCard";
+import friends from "./friends.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>South Park Click Game</Title>
+      
+      <FriendCard
+        image={friends[0].image}
+      />
+
+      <FriendCard
+        image={friends[1].image}
+      />
+
+      <FriendCard
+        image={friends[2].image}
+      />
+
+      <FriendCard
+        image={friends[3].image}
+      />
+
+      <FriendCard
+        image={friends[4].image}
+      />
+
+      <FriendCard
+        image={friends[5].image}
+      />
+
+      <FriendCard
+        image={friends[6].image}
+      />
+
+      <FriendCard
+        image={friends[7].image}
+      />
+
+      <FriendCard
+        image={friends[8].image}
+      />
+
+      <FriendCard
+        image={friends[9].image}
+      />
+
+      <FriendCard
+        image={friends[10].image}
+      />
+
+      <FriendCard
+        image={friends[11].image}
+      />
+
+
+    </Wrapper>
   );
 }
 
---
+++ src/components/FriendCard/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+function FriendCard(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default FriendCard;
